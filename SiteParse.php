5062904e9d387537bd91c137a7d6b29823a0e418
tricoadmin@brynmawr.edu <tricoadmin@brynmawr.edu@ff9f49f4-261b-11df-a3ec-b365d55abd7c>
2010-03-03 20:05:24 +0000

    - Generic version
    git-svn-id: http://mobilecat.googlecode.com/svn/trunk@5 ff9f49f4-261b-11df-a3ec-b365d55abd7c

--- SiteParse.php
+++ SiteParse.php
@@ -4,67 +4,21 @@ require_once('IIIParse.php');
 
 class SiteParse extends IIIParse {
 
-    public $catalog_url = "http://tripod.brynmawr.edu";
-    #public $base_url    = "http://tricycle.brynmawr.edu/bseitzmobile/";
-    public $base_url    = "http://m.tripod.brynmawr.edu/";
+    public $catalog_url = "http://www.example.com/";
+    public $base_url    = "http://m.example.com/";
 
-    public $catalog_name = "Tripod";
-    public $def_scope    = "10";
+    public $catalog_name = "Library Catalog";
+    public $def_scope    = "1";
 
-    public $email_from   = "tripod@trilogy.brynmawr.edu";
-    public $feedback_email   = "tricomobile@brynmawr.edu";
+    public $email_from   = "library@example.com";
+    public $feedback_email   = "feedback@example.com";
 
-    public static $cover_image_type = "syndetics";
-    public static $syndetics_client = "trico";
-    #public static $cover_image_type = "openlibrary";
+    public static $cover_image_type = "openlibrary";
+    
+    #public static $cover_image_type = "syndetics";
+    #public static $syndetics_client = "CLIENTID";
 
-    public $email_subject_prefix = "Tripod";
-
-    public $material_types = array(
-        "BOOKS"             => "Book",
-        "DOC DISCARD"       => "",
-        "MUSIC SCORES"      => "Score",
-        "MUSIC CD"          => "CD",
-        "MAPS"              => "Map",
-        "OTHER MEDIA"       => "",
-        "OTHER MUSIC"       => "Audio",
-        "SPOKEN RECORD"     => "Audio Book",
-        "E-JOURNALS"        => "E-Journals",
-        "PICTURES"          => "Picture",
-        "MUSIC LP"          => "LP",
-        "COMPUTER FILE"     => "Electronic Resource",
-        "KIT"               => "Kit",
-        "ARCHIVES/MSS"      => "Archives/Manuscripts",
-        "OBJECTS"           => "Object",
-        "JOURNAL/SERIAL"    => "Serial",
-        "MSS CODEX"         => "Manuscript",
-        "E-MUSIC"           => "E-Music",
-        "VHS"               => "VHS",
-        "E-BOOKS"           => "E-Book",
-        "DVD"               => "DVD",
-        "PHOTOGRAPHS"       => "Photograph",
-        "LASER DISC"        => "Laser Disc",
-    ); 
-
-    protected function process_link($a) {
-        $href = $a->href;
-        $text = $a->innertext;
-
-        if (preg_match('/^Connect .*(BRYN MAWR|HAVERFORD|SWARTHMORE)/', $text, $matches)) {
-            $college = ucwords(strtolower($matches[1]));
-            $text = "Connect from $college";
-        }
-
-        return array($href, $this->shorten_value($text));
-    }
-
-    protected function translate_material($material) {
-        if ($val = ar_get($material, $this->material_types)) {
-            return $val;
-        } else {
-            return ucwords(strtolower($material));
-        }
-    }
+    public $email_subject_prefix = "Library";
 }
 
 ?>
