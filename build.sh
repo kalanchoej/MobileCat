1a2aaab8baea46f3ed47fb49c828b74b018b4b23
tricoadmin@brynmawr.edu <tricoadmin@brynmawr.edu@ff9f49f4-261b-11df-a3ec-b365d55abd7c>
2010-03-03 20:32:03 +0000

    Adding build script
    git-svn-id: http://mobilecat.googlecode.com/svn/trunk@7 ff9f49f4-261b-11df-a3ec-b365d55abd7c

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VERSION=1.0
+
+rm -rf mobilecat-$VERSION
+mkdir mobilecat-$VERSION
+
+rsync -a --exclude='mobilecat-*.tar.gz' --exclude='build.sh' --exclude='*.svn' --exclude="mobilecat-$VERSION" . mobilecat-$VERSION
+
+tar --file=mobilecat-$VERSION.tar.gz --gzip --create mobilecat-$VERSION
+rm -rf mobilecat-$VERSION
+
+echo
+echo "CREATED mobilecat-$VERSION.tar.gz"
