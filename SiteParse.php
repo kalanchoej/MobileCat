60ffbce9acbfb5977b722ce14c173ab605f12fe1
dmh4@stmarys-ca.edu <dmh4@stmarys-ca.edu@ff9f49f4-261b-11df-a3ec-b365d55abd7c>
2010-07-20 18:32:36 +0000

    Added support for cover provders ContentCafe and the Google Books API. Changed default scope to 0; ensures all searches are catalog-wide.
    git-svn-id: http://mobilecat.googlecode.com/svn/trunk@21 ff9f49f4-261b-11df-a3ec-b365d55abd7c

--- SiteParse.php
+++ SiteParse.php
@@ -8,13 +8,16 @@ class SiteParse extends IIIParse {
     public $base_url    = "http://m.example.com/";
 
     public $catalog_name = "Catalog";
-    public $def_scope    = "1";
+    public $def_scope    = "0";
 
     public $email_from   = "library@example.com";
     public $feedback_email   = "feedback@example.com";
 
     public static $cover_image_type = "openlibrary";
-    
+    #public static $cover_image_type = "googlebooks"; 
+    #public static $cover_image_type = "contentcafe";
+    #public static $cc_user = "CONTENTCAFEUSERNAME";                                                                
+    #public static $cc_pass = "CONTENTCAFEPASSWORD";
     #public static $cover_image_type = "syndetics";
     #public static $syndetics_client = "CLIENTID";
 
--- cover_image.php
+++ cover_image.php
@@ -13,9 +13,27 @@
             $client = SiteParse::$syndetics_client;
             $url = "http://www.syndetics.com/index.php?isbn=$isbn/sc.gif&client=$client";
         } elseif (SiteParse::$cover_image_type == "openlibrary") {
-            $url = "http://covers.openlibrary.org/b/isbn/$isbn-S.jpg";
-        }
+            $url = "http://covers.openlibrary.org/b/isbn/$isbn-S.jpg";  #api documentation is wrong; this returns a redirect that must be parsed
+            $resp = http_parse_message(http_get($url));
+            $url = $resp->headers['Location'];
+        } elseif (SiteParse::$cover_image_type == "contentcafe") {
+            $user = SiteParse::$cc_user;
+            $pass =  SiteParse::$cc_pass;
+            $url = "http://images.btol.com/ContentCafe/Jacket.aspx?UserID=$user&Password=$pass&Return=1&Type=S&Value=$isbn&erroroverride=1&";
+        } elseif (SiteParse::$cover_image_type == "googlebooks") {	
+	        $url = "http://books.google.com/books?jscmd=viewapi&bibkeys=$isbn&callback=ProcessGBSBookInfo"; 
+	        $resp = http_parse_message(http_get($url));
+	   
+	        #ugly regex business to strip the callback and its parens; i can do better
+	        $matches = preg_split('/\(/', $resp->body);
+            $matches = preg_split('/\)/', $matches[1]);	
 
+            #decode json response and assign the image url
+    	    $results = json_decode($matches[0], true);
+	        $details = $results[$isbn];
+	        $url = $details['thumbnail_url'];
+	    }
+	
         if ($url) {
             $resp = http_parse_message(http_get($url));
         } else {
@@ -25,14 +43,15 @@
         $image = imagecreatefromstring($resp->body);
         if (imagesx($image) > 1) {
             header(sprintf("Content-Type: %s", $resp->headers['Content-Type']));
-            echo $resp->body;
+	        $matches = preg_split('/(\<!DOCTYPE|\<html)/', $resp->body);	#strip excess html returned in the body of the request (contentcafe)
+	        echo $matches[0];
         } else {
             throw new Exception("Image not found.");
         }
 
     // For any exception, return a blank image
     } catch (Exception $e) {
-        header("Content-Type: image/jpeg");
+	    header("Content-Type: image/jpeg");
         echo file_get_contents("static/nocover.jpg");
     }
 ?>
